804a9aa27dc8dbb00f89b4b13d8936ec0a9a2a81
NdaruPradana <ndarupradanagatotsuseno@gmail.com>
2025-05-27 14:40:23 +0700

    Add files via upload

---
+++ src/pekan6/NodeDLL.java
@@ -0,0 +1,15 @@
+package pekan6;
+
+public class NodeDLL {
+	//Ndaru Pradana Gatot Suseno
+	//2411532007
+int data;
+NodeDLL next;
+NodeDLL prev;
+
+public NodeDLL(int data) {
+	this.data = data;
+	this.next = null;
+	this.prev = null;
+}
+}
